065b31a2fc8e6e328ad20f6525a4121d135f0782
yuhur <yuhur1985@gmail.com>
2023-11-13 12:03:16 +0200

    AUTH with protected routes

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Login() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2> Login</h2>
