ded5244df77873ba1725e80d1494166c53253355
yuhur <yuhur1985@gmail.com>
2023-12-04 20:37:02 +0200

    Slider, more tests, ppr

--- __test__/Home.test.tsx
+++ __test__/Home.test.tsx
@@ -1,35 +1,23 @@
 //https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
-import './setupTests.mock'
-
 import { render, screen } from '@testing-library/react'
 import Home from '@/app/page'
-import { FirebaseOptions } from '@firebase/app'
-import { FirebaseApp, initializeApp } from 'firebase/app'
-import { FirebaseStorage, getStorage } from '@firebase/storage'
 
-// Your web app's Firebase configuration
-const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
-}
-
-// Initialize Firebase
-const app: FirebaseApp = initializeApp(firebaseConfig)
-
-export const storage: FirebaseStorage = getStorage(app)
+jest.mock('../src/modules/homePageSlider', () => {
+  // Replace with the actual path to your FirebaseComponent
+  return {
+    __esModule: true,
+    default: () => <div>Firebase component mock</div>,
+  }
+})
 
 it('should have H1 Home', () => {
-  render(<Home />) //ARRANGE
+  render(<Home />)
 
-  const elem: HTMLParagraphElement = screen.getByRole('heading', {
+  const elem = screen.getByRole('heading', {
     name: 'Home',
-  }) //ACT
+  })
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-expect-error
-  expect(elem).toBeInTheDocument() //ASSERT
+  expect(elem).toBeInTheDocument()
 })
