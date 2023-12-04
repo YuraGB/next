303afb3d531c53c846ef051af7592c575c023fe4
yuhur <yuhur1985@gmail.com>
2023-12-04 21:05:31 +0200

    Slider, more tests, ppr

---
+++ cypress/support/index.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app'
+import 'firebase/storage'
+import { FirebaseOptions } from '@firebase/app'
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
