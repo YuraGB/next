ab8ed2120a024c5b03d02ebbd7d4833cfefbdbfa
yuhur <yuhur1985@gmail.com>
2023-12-04 20:54:48 +0200

    Slider, more tests, ppr

--- src/modules/homePageSlider/__test__/Carousel.test.tsx
+++ src/modules/homePageSlider/__test__/Carousel.test.tsx
@@ -1,15 +1,50 @@
 import '@/../__test__/setupTests.mock'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import HomePageSlider from '@/modules/homePageSlider'
+// Import the necessary modules and types
+import { listAll, getDownloadURL, StorageReference } from '@firebase/storage'
+
+// Type for the mock implementation
+type StorageMock = jest.Mocked<typeof import('@firebase/storage')>
+
+// Mock the Firebase storage module
+jest.mock('@firebase/storage')
+
+// Mock the specific functions you use
+const mockedListAll = listAll as StorageMock['listAll']
+const mockedGetDownloadURL = getDownloadURL as StorageMock['getDownloadURL']
+
+// Mock the behavior of listAll and getDownloadURL
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+mockedListAll.mockImplementation((ref: StorageReference) => {
+  // Implement your mock behavior for listAll
+  return Promise.resolve({
+    items: [
+      /* mock StorageReference objects */
+    ],
+  })
+})
+
+mockedGetDownloadURL.mockImplementation((storageRef: StorageReference) => {
+  // Implement your mock behavior for getDownloadURL
+  return Promise.resolve('https://example.com/mock-image.jpg')
+})
+
+// Import the function that uses Firebase
+
+// Your actual test
 
 describe('Slider tests', () => {
-  it('Props photo will be null', () => {
+  it('Props photo will be null', async () => {
     render(<HomePageSlider />) //ARRANGE
 
     const elem = screen.queryByAltText('Yuhur photo') //ACT
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    expect(elem).not.toBeInTheDocument() //ASSERT
+    await waitFor(
+      () =>
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        expect(elem).not.toBeInTheDocument() //ASSERT
+    )
   })
 })
