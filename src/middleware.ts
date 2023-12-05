d2554f7453b09a26200d38cf3ad016d6678c91d4
yuhur <yuhur1985@gmail.com>
2023-12-05 18:54:48 +0200

    wrong beh at the orid

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,6 +24,23 @@
 // Ref: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 // export const config = { matcher: ['/admin'] }
 
-export { default } from 'next-auth/middleware'
+import { NextRequestWithAuth, withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(request: NextRequestWithAuth) {
+    if (
+      request.nextUrl.pathname.startsWith('/admin') &&
+      request.nextauth.token?.role !== 'admin'
+    ) {
+      return NextResponse.rewrite(new URL('/denied', request.url))
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+)
 
 export const config = { matcher: ['/admin'] }
