e22909d1714ba40f5f1012a01ba42240c525eb04
yuhur <yuhur1985@gmail.com>
2023-12-13 20:37:36 +0200

    fix: regrations

--- src/modules/homePageSlider/useHPSlider.ts
+++ src/modules/homePageSlider/useHPSlider.ts
@@ -1,11 +1,10 @@
-import { useMemo, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { getPhotos } from '@/modules/homePageSlider/api/getPhotos'
 
 export const useHPSlider = () => {
   const [photos, setPhotos] = useState<string[]>([])
-  useMemo(async () => {
-    const images: string[] = await getPhotos('/images')
-    setPhotos(images)
+  useEffect(() => {
+    getPhotos('/images').then((images: string[]) => setPhotos(images))
   }, [])
 
   return { photos }
