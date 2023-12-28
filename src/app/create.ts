138c393a5e49f2ef07202dff35dd486044ff5338
yuhur <yuhur1985@gmail.com>
2023-12-28 20:05:32 +0200

    feat: HP slider from firefox to prisma as source

--- src/app/create.ts
+++ src/app/create.ts
@@ -2,11 +2,7 @@
 // import prisma from '../../lib/prisma'
 
 // export const create = async () => {
-//   const array = [
-//     'https://firebasestorage.googleapis.com/v0/b/next-8767c.appspot.com/o/images%2Fimage1.webp?alt=media&token=7f7e0245-537f-44eb-8d54-5f06cfd2c20b',
-//     'https://firebasestorage.googleapis.com/v0/b/next-8767c.appspot.com/o/images%2Fimage2.webp?alt=media&token=a1ad3674-4188-4c0d-aedd-e0f3d9c023f6',
-//     'https://firebasestorage.googleapis.com/v0/b/next-8767c.appspot.com/o/images%2Fimage3.webp?alt=media&token=b3861d57-3599-4686-910d-2f4ce6be56ef',
-//   ]
+
 //
 //   array.map((e) => {
 //     prisma?.slide
@@ -27,7 +23,7 @@
 //       sliderID: '1321321',
 //       slide: {
 //         connect: {
-//           id: '658da5ab8d1a2e2e4f81f117',
+//           id: '',
 //         },
 //       },
 //     },
