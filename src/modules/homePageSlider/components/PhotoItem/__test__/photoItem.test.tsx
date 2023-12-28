16c7b3f425a467a26e2286322c21a9b1ec8ebed2
yuhur <yuhur1985@gmail.com>
2023-12-28 20:28:19 +0200

    feat: HP slider from firefox to prisma as source

--- src/modules/homePageSlider/components/PhotoItem/__test__/photoItem.test.tsx
+++ src/modules/homePageSlider/components/PhotoItem/__test__/photoItem.test.tsx
@@ -2,9 +2,14 @@ import { render, screen } from '@testing-library/react'
 import PhotoItem from '@/modules/homePageSlider/components/PhotoItem/PhotoItem'
 
 describe('PhotoItem tests', () => {
-  const dummyPhotoUrl =
-    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Furl%2F&psig=AOvVaw3oiFcQx0aQynSdpACVBZGE&ust=1701791177774000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjt4aeQ9oIDFQAAAAAdAAAAABAD'
-
+  const dummyPhotoUrl = {
+    id: '1',
+    description: 'description',
+    title: 'title',
+    url: 'url',
+    image:
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Furl%2F&psig=AOvVaw3oiFcQx0aQynSdpACVBZGE&ust=1701791177774000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIjt4aeQ9oIDFQAAAAAdAAAAABAD',
+  }
   it('Props photo will be null', () => {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-expect-error
