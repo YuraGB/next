eda0a51480b209e3510bbd3fcee02b3d6be86a20
yuhur <yurii.h@fisha.co.il>
2024-01-10 13:38:45 +0200

    feat: added theme provider and theme switcher

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -5,7 +5,7 @@ describe('Navigation', () => {
   })
   it('should navigate to login page', () => {
     // Start from the index page
-    cy.visit('http://localhost:3000/')
+    cy.visit('http://localhost:8000/')
 
     // Find a link with an href attribute containing "login" and click it
     cy.get('a[href*="/login"]')
