f8fbb888cc324e9ed3aa375bbdb21774443b3376
yuhur <yurii.h@fisha.co.il>
2024-01-14 18:52:03 +0200

    fix: prisma, translates, modalTales create

--- __test__/Home.test.tsx
+++ __test__/Home.test.tsx
@@ -1,6 +1,10 @@
 //https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
 import { render, screen, waitFor } from '@testing-library/react'
 import Home from '@/app/[locale]/page'
+import { createIntl } from '@formatjs/intl'
+import en from '@/i18n/en.json'
+import { ReactNode } from 'react'
+import ServerIntlProvider from '@/app/context/i18nProvider'
 
 jest.mock('../src/modules/homePageSlider', () => {
   // Replace with the actual path to your FirebaseComponent
@@ -11,7 +15,22 @@ jest.mock('../src/modules/homePageSlider', () => {
 })
 
 it('should have H1 Home', async () => {
-  render(<Home />)
+  const intl = createIntl({
+    locale: 'en',
+    messages: en,
+    onError: (e) => console.log(e),
+  })
+  function Wrapper({ children }: { children: ReactNode }) {
+    return (
+      <ServerIntlProvider locale={intl.locale} messages={intl.messages}>
+        {children}
+      </ServerIntlProvider>
+    )
+  }
+
+  render(<Home />, {
+    wrapper: Wrapper,
+  })
 
   const elem = screen.getByRole('heading', {
     name: 'Welcome to Fairy tale world - A Magical World of Stories and Fairy Tales for Children',
